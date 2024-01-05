a35c97981d7630828c971465c97a68254bc08f01
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2024-01-05 14:51:37 +0100

    Update Dapr configuration
    Removed the actorStateStore configuration option from the statestore-outbox.yaml file, and added configuration updates in Program.cs. Also, created a new config.yaml file that includes tracing details.

--- DaprOutbox/DaprOutbox.AppHost/Program.cs
+++ DaprOutbox/DaprOutbox.AppHost/Program.cs
@@ -3,6 +3,8 @@ using Aspire.Hosting.Dapr;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddDapr(options => options.EnableTelemetry = true);
+
 var apiService = builder.AddProject<Projects.DaprOutbox_ApiService>("apiservice");
 
 var daprResourcesPaths = ImmutableHashSet.Create("./components");
@@ -10,7 +12,8 @@ var daprResourcesPaths = ImmutableHashSet.Create("./components");
 var subscriberService = builder.AddProject<Projects.DaprOutbox_Subscriber>("subscriber")
     .WithDaprSidecar(new DaprSidecarOptions
     {
-        AppId = "subscriber",
+        AppId = "subscriber-dapr",
+        Config = "./components/config.yaml",
         ResourcesPaths = daprResourcesPaths
     });
 
@@ -18,7 +21,8 @@ builder.AddProject<Projects.DaprOutbox_Web>("webfrontend")
     .WithReference(apiService)
     .WithDaprSidecar(new DaprSidecarOptions
     {
-        AppId = "webfrontend",
+        AppId = "webfrontend-dapr",
+        Config = "./components/config.yaml",
         ResourcesPaths = daprResourcesPaths
     });
 
