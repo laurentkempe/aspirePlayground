c9de78310c5ae7b18ec85c540d7b9925c8440bc6
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2024-01-08 11:31:32 +0100

    Add CloudNative.CloudEvents package and update event handling
    The CloudNative.CloudEvents package was added to the DaprOutbox.Subscriber project to allow for more standardized cloud event handling. The event handling function for "/count" endpoint was updated, changing the input parameters to accept CloudEvent object instead of CountEvent. It now logs the incoming cloud event's data.

--- DaprOutbox/DaprOutbox.Subscriber/Program.cs
+++ DaprOutbox/DaprOutbox.Subscriber/Program.cs
@@ -1,3 +1,4 @@
+using CloudNative.CloudEvents;
 using DaprOutbox.Shared;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,9 +11,9 @@ var app = builder.Build();
 // app.UseCloudEvents();
 app.MapSubscribeHandler();
 
-app.MapPost("/count", (CountEvent countEvent, ILogger<Program> logger) =>
+app.MapPost("/count", async (HttpContext context, CloudEvent cloudEvent, ILogger<Program> logger) =>
 {
-    logger.LogInformation("Received count event: {Counter}", countEvent.Counter);
+    logger.LogInformation("Received count event: {Counter}", cloudEvent.Data);
     
 }).WithTopic("pubsub", "Counter");
 
