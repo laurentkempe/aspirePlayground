7c83b64425e8fe2700e28a989b0bdd577d6ff60a
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2024-02-03 16:12:06 +0100

    Add CountService project and update app configurations
    Introduced a new service project named CountService. Also, extended the global solution and AppHost to encompass the CountService along with Dapr components. Lastly, added corresponding references and the Dapr sidecar to the 'webfrontend' project.

--- DevApps101/DevApps101.AppHost/Program.cs
+++ DevApps101/DevApps101.AppHost/Program.cs
@@ -1,11 +1,21 @@
 var builder = DistributedApplication.CreateBuilder(args);
+builder.AddDapr();
 
+// Using Aspire Redis Component
 var cache = builder.AddRedis("cache");
 
 var apiService = builder.AddProject<Projects.DevApps101_ApiService>("apiservice");
 
+// Using Dapr
+var daprStateStore = builder.AddDaprStateStore("statestore");
+var daprPubSub = builder.AddDaprPubSub("pubsub");
+
+var countService = builder.AddProject<Projects.DevApps101_CountService>("countservice")
+    .WithDaprSidecar();
+
 builder.AddProject<Projects.DevApps101_Web>("webfrontend")
     .WithReference(cache)
-    .WithReference(apiService);
+    .WithReference(apiService)
+    .WithDaprSidecar();
 
 builder.Build().Run();
