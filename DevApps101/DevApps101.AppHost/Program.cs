ae3de625da290a0648ab2da87f3a79419897a832
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2024-02-03 17:20:29 +0100

    Remove Dapr configuration comments in Program.cs
    The commented lines related to Dapr configuration in Program.cs (in DevApps101.AppHost project) have been deleted. These commented lines were not necessary and thus removed to ensure code cleanliness and readability.

--- DevApps101/DevApps101.AppHost/Program.cs
+++ DevApps101/DevApps101.AppHost/Program.cs
@@ -6,10 +6,6 @@ var cache = builder.AddRedis("cache");
 
 var apiService = builder.AddProject<Projects.DevApps101_ApiService>("apiservice");
 
-// Using Dapr
-// var daprStateStore = builder.AddDaprStateStore("statestore");
-// var daprPubSub = builder.AddDaprPubSub("pubsub");
-
 var countService = builder.AddProject<Projects.DevApps101_CountService>("countservice")
     .WithDaprSidecar();
 
