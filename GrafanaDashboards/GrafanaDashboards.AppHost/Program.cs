bddbc9528d2ee642478a34e4dcd62c61ce401cae
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2024-02-16 13:17:21 +0100

    Enable Prometheus exporter in GrafanaDashboards
    The Prometheus exporter has been enabled in the GrafanaDashboards service by uncommenting certain code sections. A new dependency, OpenTelemetry.Exporter.Prometheus.AspNetCore, has been added to GrafanaDashboards.ServiceDefaults.csproj. A new Prometheus configuration file has also been added, and Prometheus endpoint scraping has been set up in the app's default endpoints. Lastly, a Prometheus container has been included to the distributed application builder.

--- GrafanaDashboards/GrafanaDashboards.AppHost/Program.cs
+++ GrafanaDashboards/GrafanaDashboards.AppHost/Program.cs
@@ -1,5 +1,10 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+// ⚠️ Check the port on prometheus/prometheus.yml host.docker.internal:5019
+builder.AddContainer("prometheus", "prom/prometheus")
+    .WithVolumeMount("../prometheus", "/etc/prometheus")
+    .WithEndpoint(containerPort: 9090, hostPort: 9090, scheme: "http");
+
 var apiService = builder.AddProject<Projects.GrafanaDashboards_ApiService>("apiservice");
 
 builder.AddProject<Projects.GrafanaDashboards_Web>("webfrontend")
