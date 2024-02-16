7e1eec7a9fd6b504d34a6ac2a6a7daf51c45382e
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2024-02-16 14:01:13 +0100

    Add Grafana configuration files for ASP.NET Core metrics dashboard
    New configuration files have been added to Grafana. These include the main configuration file (grafana.ini) with defined anonymous authority, the datasource configuration (default.yaml) setting Prometheus as default, and the dashboard provider configuration. These changes will enable the setup of Grafana with configured settings.
    Aslo introduces a new Grafana dashboard for displaying ASP.NET Core metrics from Prometheus. The dashboard includes various panels such as request durations, error rates, current connections, total requests, and unhandled exceptions among others. This dashboard will aid in the monitoring of ASP.NET Core applications.

--- GrafanaDashboards/GrafanaDashboards.AppHost/Program.cs
+++ GrafanaDashboards/GrafanaDashboards.AppHost/Program.cs
@@ -5,6 +5,11 @@ builder.AddContainer("prometheus", "prom/prometheus")
     .WithVolumeMount("../prometheus", "/etc/prometheus")
     .WithEndpoint(containerPort: 9090, hostPort: 9090, scheme: "http");
 
+builder.AddContainer("grafana", "grafana/grafana")
+    .WithVolumeMount("../grafana/config", "/etc/grafana")
+    .WithVolumeMount("../grafana/dashboards", "/var/lib/grafana/dashboards")
+    .WithEndpoint(containerPort: 3000, hostPort: 3000, name: "grafana-http", scheme: "http");
+
 var apiService = builder.AddProject<Projects.GrafanaDashboards_ApiService>("apiservice");
 
 builder.AddProject<Projects.GrafanaDashboards_Web>("webfrontend")
