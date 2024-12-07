d4b553847c7f4bb1c7bf93f877c79fb1c5a146de
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2024-12-07 16:33:20 +0100

    Enhanced the script to display the name of each project before starting the build process

--- BuildAll.ps1
+++ BuildAll.ps1
@@ -1,2 +1,7 @@
 # Recursively find all dotnet solution with *.sln extension and build each one
-Get-ChildItem -Recurse -Filter *.sln | ForEach-Object { & dotnet build $_.FullName -c Release }
+Get-ChildItem -Recurse -Filter *.sln | ForEach-Object {
+    Write-Host "Building project:" -NoNewline
+    Write-Host " $($_.FullName)" -ForegroundColor DarkBlue
+    & dotnet build $_.FullName -c Release
+    Write-Host
+}
